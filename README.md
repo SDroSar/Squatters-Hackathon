2a4d901d80cf3bccef09547b4ef480672f774710
SDroSar <ssaran@student.unimelb.edu.au>
2024-04-04 15:17:03 +1100

    Added files

---
+++ README.md
@@ -0,0 +1,30 @@
+# MERN Stack Project - Base Code
+
+This branch contains the base code for the MERN stack project.
+We will create a simple signup page which can record the users who sign up.
+Workshop participants will fill the backend .js files during the workshop.
+Frontend code is given, so please refer to the frontend workshop for more information.
+
+## Requirements
+
+https://docs.google.com/document/d/1dHABryWlVxH1-qq496gtM8VWi9koev5w1-OYemD84ME/edit?usp=sharing
+
+## Download
+To get started with the base code, clone the repository and check out the `main` branch:
+
+```
+git clone https://github.com/KaiKaiai/codebrew2024-backend-workshop.git
+git checkout main
+```
+OR
+
+To get started with the base code, download as a ZIP to extract into VSCode
+
+## Resources
+
+For recording + other Resources:
+https://drive.google.com/drive/folders/1ADf1ZDk6daVeGfpqKdLTKo_jJiZAxen4?usp=sharing
+
+## Issues
+
+Please let one of our workshop helpers know, thanks.
---
+++ frontend/src/SignupForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignupForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const res = await axios.post('http://localhost:5000/api/users/signup', { username, password });
+      console.log('Signup Success:', res.data);
+    } catch (error) {
+      if (error.response) {
+        console.error('Signup Error:', error.response.data);
+      } else if (error.request) {
+        console.error('No response received:', error.request);
+      } else {
+        console.error('Error', error.message);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignupForm;
