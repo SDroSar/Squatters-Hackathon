23da68b1645f249212161c312a479c481d60d66f
SDroSar <ssaran@student.unimelb.edu.au>
2024-04-06 16:00:57 +1100

    dummy map works

--- frontend/src/components/DummyMap.js
+++ frontend/src/components/DummyMap.js
@@ -13,10 +13,33 @@ const center = {
 };
 
 const locations = [
-  { lat: 50.0411867, lng: 21.9990196, title: 'Rzeszow', description: 'This is Rzeszow' },
-  { lat: 51.2464536, lng: 22.5684463, title: 'Lublin', description: 'This is Lublin' },
-  { lat: 50.7226256, lng: 23.2514861, title: 'Zamosc', description: 'This is Zamosc' },
-  { lat: 50.0619474, lng: 19.9368564, title: 'Krakow', description: 'This is Krakow' }
+  { lat: 50.0411867, lng: 21.9990196, title: 'Rzeszow', address: 'Al. Józefa Piłsudskiego 34 35-001 Rzeszów Poland',  telephone: '+48 17 875 47 00',
+  resources: {
+    foodPacks: 100,
+    beds: 100,
+    powerPacks: 100
+  } },
+  { lat: 51.2464536, lng: 22.5684463, title: 'Lublin',  address: 'ul. Krakowskie Przedmieście 54 20-002 Lublin Poland',
+  telephone: '+48 81 448 08 00',
+  resources: {
+    foodPacks: 100,
+    beds: 100,
+    powerPacks: 100
+  } },
+  { lat: 50.7226256, lng: 23.2514861, title: 'Zamosc',address: 'ul. Gminna 9 22-400 Zamość Poland',
+  telephone: '+48 84 530 09 10',
+  resources: {
+    foodPacks: 100,
+    beds: 100,
+    powerPacks: 100
+  } },
+  // { lat: 50.0619474, lng: 19.9368564, title: 'Krakow', address: 'ul. Świętokrzyska 12 31-015 Kraków Poland',
+  // telephone: '+48 12 428 64 00',
+  // resources: {
+  //   foodPacks: 100,
+  //   beds: 100,
+  //   powerPacks: 100
+  // } }
 ];
 
 function MyComponent() {
@@ -69,6 +92,11 @@ function MyComponent() {
           <div>
             <h3>{selectedLocation.title}</h3>
             <p>{selectedLocation.description}</p>
+            <p>{selectedLocation.address}</p>
+            <p>{selectedLocation.telephone}</p>
+            <p>Food Packs: {selectedLocation.resources.foodPacks}</p>
+            <p>Beds: {selectedLocation.resources.beds}</p>
+            <p>Power Packs: {selectedLocation.resources.powerPacks}</p>
           </div>
         </InfoWindow>
       )}
